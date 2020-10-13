6218383359febcb378442dd110bda35ac3541483
Kiril <kiril@olgas-air.mynet>
2020-10-13 16:00:17 +0100

    created a new erb file with cat form and changed /named-cat route to post

--- app.rb
+++ app.rb
@@ -29,10 +29,14 @@ set :session_secret, 'super secret'
     erb(:index)
   end
 
-  get '/named-cat' do
+  post '/named-cat' do
     p params
     @name = params[:name]
     erb(:index)
+  end
+
+  get '/cat-form' do
+    erb(:cat_form)
   end
 
 
